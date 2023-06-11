786081c04f889331af66094407aa87e602264f0f
Turu0512 <hinataptyan@gmail.com>
2023-06-12 07:57:58 +0900

    １を渡すと１を返すテストコードを記述（失敗する）

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,5 +1,8 @@
 RSpec.describe "FizzBuzz" do
   describe "fizz_buzz" do
-    
+    it "1を渡すと、'1'を返す" do
+      result = FizzBuzz.new.fizz_buzz(1)
+      expect(result).to eq('1')
+    end
   end
 end
